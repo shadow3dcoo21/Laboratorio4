656e8c8ecd0d55ffe1226d815ecaa86e2a45876e
Farid Lucio Gonzales Gonzalo <farid.gonzales@tecsup.edu.pe>
2024-04-08 07:21:34 -0500

    Lab4

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+// Definir las rutas
+app.get('/', (req, res) => {
+  res.send('Laboratorio 4');
+});
+
+app.get('/clientes', (req, res) => {
+  const clientes = ['Farid Lucio Gonzales', 'Linda Mercedez Zegarra', 'Karolay Gallegos'];
+  res.json(clientes);
+});
+
+app.get('/productos', (req, res) => {
+  const productos = ['Televisor 42 pulgadas', 'Camara Cannon', 'Mouse inalambrico'];
+  res.json(productos);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
